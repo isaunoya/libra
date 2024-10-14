b29d92d35bbf7c5f231bde0a9f74ef56efaaddde
isaunoya <isaunoya@qq.com>
2024-10-14 22:12:53 +0800

    a

--- noya/head.hpp
+++ noya/head.hpp
@@ -56,15 +56,16 @@ template <typename T, typename T2> void cmin(T &x, const T2 &y) {
 template <typename T, typename T2> void cmax(T &x, const T2 &y) {
   x = x > y ? x : y;
 }
-using ll = int64_t;
+
+#include "noya/limit.hpp"
+
 using vi = vector<int>;
 using vl = vector<ll>;
-using ull = uint64_t;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 template <class T> using vc = vector<T>;
-const int INF = 1000000000;
-const ll LNF = 1000000000000000000;
+const int INF = 1010000000;
+const ll LNF = 1010000000000000000;
 #define sz(x) int((x).size())
 #define all(x) begin(x), end(x)
 #define fi first
