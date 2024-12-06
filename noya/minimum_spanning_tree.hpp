a5bba92e2daacac4cd173fe7487da2306e749622
isaunoya <isaunoya@qq.com>
2024-12-06 10:39:20 +0800

    add prim

--- noya/minimum_spanning_tree.hpp
+++ noya/minimum_spanning_tree.hpp
@@ -3,7 +3,6 @@
 
 #include "atcoder/dsu.hpp"
 #include <numeric>
-
 // @(w, u, v)
 template <class T>
 std::pair<int64_t, std::vector<int>>
@@ -56,5 +55,57 @@ maximum_spanning_tree(int N, const std::vector<T> &edges) {
   return make_pair(ans, idx);
 }
 
+#include "noya/limit.hpp"
+// @(w, u, v)
+template <class T>
+std::pair<int64_t, std::vector<int>> prim_dense(int N,
+                                                const std::vector<T> &edges) {
+  std::vector<std::vector<int>> id(N, std::vector<int>(N, -1));
+  const int M = int(edges.size());
+  for (int i = 0; i < M; i++) {
+    auto [w, u, v] = edges[i];
+    assert(0 <= u && u < N);
+    assert(0 <= v && v < N);
+    id[u][v] = id[v][u] = i;
+  }
+  std::vector<int> idx;
+  std::vector<int> vis(N, 0);
+  std::vector<int> dis(N, -1);
+  auto cmp = [&](int a, int b) -> int {
+    if (a == -1)
+      return b;
+    if (b == -1)
+      return a;
+    auto [w1, u1, v1] = edges[a];
+    auto [w2, u2, v2] = edges[b];
+    return w1 < w2 ? a : b;
+  };
+
+  int64_t ans = 0;
+  int k = 0;
+  for (int t = 1; t < N; t++) {
+    vis[k] = 1;
+    int nx = -1;
+    for (int i = 0; i < N; i++) {
+      if (!vis[i]) {
+        dis[i] = cmp(dis[i], id[k][i]);
+        nx = cmp(nx, dis[i]);
+      }
+    }
+    if (nx == -1)
+      break;
+    idx.push_back(nx);
+    {
+      auto [w, u, v] = edges[nx];
+      ans += w;
+      if (vis[u]) {
+        k = v;
+      } else {
+        k = u;
+      }
+    }
+  }
+  return make_pair(ans, idx);
+}
 
 #endif // NOYA_MINIMUM_SPANNING_TREE_HPP
